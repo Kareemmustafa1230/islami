8532343c597ec32e76dcd74c015ebfc09c3d6cb2
ZBOOK <kareemmustafa1230@gmail.com>
2023-09-19 23:53:39 +0300

    initial project. Added HomeScreen.

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:islami/HomeScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute:HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName : (context) => HomeScreen(),
+      },
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
